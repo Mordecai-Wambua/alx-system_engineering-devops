afffe4ff43572b9dd4557f7558f225bf174d8469
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-04-20 00:27:41 +0300

    puppet tasks

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # install flask version 2.1.0 from pip3
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/bin', '/usr/bin']
 }
