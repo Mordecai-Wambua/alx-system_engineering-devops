3df4182270e4250e9c2a0dbca7c125b2c6f0b1a3
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-04-20 09:45:18 +0300

    corrections

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # install flask version 2.1.0 from pip3
 
 exec { 'install_flask':
+  require => Exec['python-check'],
   command => '/usr/bin/pip3 install flask==2.1.0',
   path    => ['/bin', '/usr/bin']
+}
+
+exec {'python-check':
+  command => '/usr/bin/which python3'
 }
