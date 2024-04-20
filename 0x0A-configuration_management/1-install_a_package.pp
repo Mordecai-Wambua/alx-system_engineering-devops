6244a6b4dbbe6fb6c95f52da5b540e484bf667ae
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-04-20 09:49:15 +0300

    corrections

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,5 @@
 # install flask version 2.1.0 from pip3
 
 exec { 'install_flask':
-  require => Exec['python-check'],
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/bin', '/usr/bin']
-}
-
-exec {'python-check':
-  command => '/usr/bin/which python3'
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
