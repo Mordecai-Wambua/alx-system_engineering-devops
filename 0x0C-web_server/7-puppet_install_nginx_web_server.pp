fdad12ddd4d6d54b1efdcd473c88ceb226839ba4
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-04-28 18:53:00 +0300

    task 5: Puppet implementation of nginx install and redirect 301

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -9,14 +9,16 @@ package { 'nginx':
   require => Exec['update server']
 }
 
-file { 'html':
-  path    => '/var/www/html/index.html',
+file { '/var/www/html/index.html':
+  ensure  => 'file',
   content => 'Hello World!'
 }
 
-exec { 'redirects':
-  command  => sed -i '/listen 80 default_server/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default,
-  provider => 'shell'
+file_line { 'redirect':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
 }
 
 service { 'nginx':
