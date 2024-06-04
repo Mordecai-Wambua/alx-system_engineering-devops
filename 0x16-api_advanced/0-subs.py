720e946fc8592a606cce568e4012e1a16fa0b922
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-06-04 19:21:02 +0300

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,9 +5,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return number if subscribers for given subreddit."""
-    url = "https://www.reddit.com/r/"
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'my_app/0.0.1 (by /u/Crazy_Bowler_5441)'}
-    response = requests.get(url+f'{subreddit}/about.json', headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         dic = response.json()
