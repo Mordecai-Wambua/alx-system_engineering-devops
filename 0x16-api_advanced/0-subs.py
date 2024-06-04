dd6844fc19b2b6a25a80ba5f1b1578a436beb3ea
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-06-04 19:27:47 +0300

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""Reddit API: Finding subscribers."""
+"""
+Reddit API usage for finding subscribers.
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """Return number if subscribers for given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         dic = response.json()
         output = dic['data']['subscribers']
-        return(output)
+        return output
     else:
-        return(0)
+        return 0
