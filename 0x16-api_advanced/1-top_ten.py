43495f09961703e18b95b49247f4ed7fcc8e6c4d
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-06-05 11:46:03 +0300

    fixes

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""
-Query Reddit API for frist 10 subreddit posts.
-"""
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles for top posts."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
-    headers = {"User-Agent": "Mozilla/5.0"}
-    params = {"limit": 10}
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
     if response.status_code == 404:
         print("None")
         return
-
-    data = response.json().get('data')
-    [print(post.get('data').get('title')) for post in data.get('children')]
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
