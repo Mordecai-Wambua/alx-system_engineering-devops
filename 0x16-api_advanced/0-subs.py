be7d2c6400ddb403d22785b82079b893c3c8f138
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-06-05 11:35:55 +0300

    fixes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Reddit API usage for finding subscribers.
-"""
+"""Reddit API usage for finding subscribers."""
 import requests
 
 
@@ -10,9 +8,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    if response.status_code == 404:
         return 0
+    data = response.json().get("data")
+    return data.get("subscribers")
