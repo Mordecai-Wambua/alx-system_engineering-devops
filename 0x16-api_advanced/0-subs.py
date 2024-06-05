8cf2d897b830a664f36205d6d8c4cdc333a36e4f
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-06-05 11:39:05 +0300

    fixes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,10 @@ import requests
 def number_of_subscribers(subreddit):
     """Return number if subscribers for given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {
+            "User-Agent":
+            "Linux:0x16.api.advanced:v1.0.0 (by /u/Crazy_Bowler_5441"
+            }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
